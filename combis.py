24ba1137c80a360fd0d17bed65ba8fe104b4d80c
IDH <idh@neasenergy.com>
2020-03-17 19:50:13 +0100

    script+initial_vals

---
+++ combis.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 17 11:34:56 2020
+
+@author: idh
+"""
+import os
+import itertools as it
+import numpy as np
+import pandas as pd
+from collections import namedtuple
+
+titan = namedtuple("Titan", ["name", 'type', 'role'])
+
+Moloch = titan("Moloch", "Fire", ["Tank"])
+Sigurd = titan("Sigurd", "Water", ["Tank"])
+Angus = titan("Angus", "Earth", ["Tank"])
+
+Eden = titan("Eden", "Earth", ["Damage", "Utility"])
+Hyperion = titan("Hyperion", "Water", ["Damage", "Utility"])
+Araji = titan("Araji", "Fire", ["Damage"])
+
+Mairi = titan("Mairi", "Water", ["Utility"])
+Avalon = titan("Avalon", "Earth", ["Utility"])
+Ignis = titan("Ignis", "Fire", ["Utility"])
+
+Nova = titan("Nova", "Water", ["Damage"])
+Vulcan = titan("Vulcan", "Fire", ["Damage"])
+Sylva = titan("Sylva", "Earth", ["Damage"])
+
+tanks = [Moloch, Sigurd, Angus]
+damage = [Eden, Hyperion, Araji, Nova, Vulcan, Sylva]
+utility = [Eden, Hyperion, Nova, Mairi, Avalon, Ignis]
+
+titans = []
+_ = [titans.append(ttn) for ttn in tanks+damage+utility if not ttn in titans]
+
+
+def check_for_match(ttn_list1, ttn_list2):
+    return np.array([ttn in ttn_list2 for ttn in ttn_list1]).all()
+
+def names_only(ttn_list):
+    return [ttn.name for ttn in ttn_list]
+
+def check_amount_tanks(ttn_list):
+    return sum(["Tank" in ttn.role for ttn in ttn_list])
+
+def check_amount_utility(ttn_list):
+    return sum(["Utility" in ttn.role for ttn in ttn_list])
+
+def check_amount_damage(ttn_list):
+    return sum(["Damage" in ttn.role for ttn in ttn_list])
+
+def check_amount_fire(ttn_list):
+    return sum(["Fire" in ttn.type for ttn in ttn_list])
+
+def check_amount_water(ttn_list):
+    return sum(["Water" in ttn.type for ttn in ttn_list])
+
+def check_amount_earth(ttn_list):
+    return sum(["Earth" in ttn.role for ttn in ttn_list])
+
+def totem_active(ttn_list):
+    return np.array([func(ttn_list) >=3 for func in 
+                     [check_amount_fire, check_amount_water, check_amount_earth]]).any()
+    
+def print_summary_match(ttn_list1, ttn_list2):
+    print("Team defending: {}\ntotem: {}\ntanks: {}\nutility: {}\ndamage: {}\n\n\
+Team attacking: {}\ntotem: {}\ntanks: {}\nutility: {}\ndamage: {}".format(
+          names_only(ttn_list1),
+          totem_active(ttn_list1),
+          check_amount_tanks(ttn_list1),
+          check_amount_utility(ttn_list1),
+          check_amount_damage(ttn_list1),
+          names_only(ttn_list2),
+          totem_active(ttn_list2),
+          check_amount_tanks(ttn_list2),
+          check_amount_utility(ttn_list2),
+          check_amount_damage(ttn_list2))
+          )
+          
+#TODO refactor repetitive statements to something prettier..
+combinations = [draw for draw in it.combinations(titans, 5)]
+outcomes = {"y": 1, "n": -1}
+if "current_state.csv" not in os.listdir():
+    init = np.zeros(len(combinations), dtype=int)
+    grid = pd.DataFrame({", ".join(names_only(c)): init for c in combinations})
+    grid.index = grid.columns
+    grid.index.name = "titan combination"
+else:
+    grid = pd.read_csv("current_state.csv", decimal=",", sep=";").set_index("titan combination")
+for i,c1 in enumerate(combinations):
+    for j,c2 in enumerate(combinations):
+        if grid.iloc[j,i] != 0:
+            continue
+        print_summary_match(c1,c2)
+        result = input("Did the attacking team win? [y/n]").lower()
+        while result not in outcomes.keys():
+            print("Answer not recognized...")
+            result = input("Did the attacking team win? [y/n]").lower()
+        result = int(outcomes[result])
+        grid.iloc[j,i] = result
+        cont = input("Want to continue now? [y/n]").lower()
+        while cont not in outcomes.keys():
+            print("Answer not recognized...")
+            cont = input("Want to continue now? [y/n]").lower()
+        if cont == "n":
+            break
+    if cont == "n":
+        break
+            
+grid.to_csv("current_state.csv", decimal=',', sep=';')
